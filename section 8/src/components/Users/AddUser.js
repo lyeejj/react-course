3ecf7c40274570ed521615a82520ff635b90a77f
yjlee1012 <elj308@naver.com>
2022-10-11 15:05:57 +0900

    사용자 입력 state 관리하기

--- section 8/src/components/Users/AddUser.js
+++ section 8/src/components/Users/AddUser.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     //submit 기본적인 이벤트 reloading 막기위함
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const nameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={nameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
