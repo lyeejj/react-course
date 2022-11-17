0ba964185c9781a35c3031a7c103de6db1959e79
yjlee1012 <elj308@naver.com>
2022-11-17 22:41:05 +0900

    장바구니 Cart Context 추가

--- Practice Project(Building a Food Order App)/src/App.js
+++ Practice Project(Building a Food Order App)/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,13 +15,13 @@ function App() {
 
   // useEffect();
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
