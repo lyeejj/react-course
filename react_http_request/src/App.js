f43f03f34ac7f9a65ce5d5b19bb8f7eb859b0f40
yjlee1012 <elj308@naver.com>
2022-11-30 18:43:26 +0900

    Promise를 async/await로 변환

--- react_http_request/src/App.js
+++ react_http_request/src/App.js
@@ -5,22 +5,19 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const res = await fetch("https://swapi.dev/api/films/");
+    const data = await res.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
   return (
     <React.Fragment>
