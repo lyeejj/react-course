ca4a67b91142363b01c6f820782d079b36f27e41
yjlee1012 <elj308@naver.com>
2022-12-19 01:33:16 +0900

    코드 분할하기

--- practice Redux/src/store/index.js
+++ practice Redux/src/store/index.js
@@ -1,54 +1,11 @@
-import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
   // merge 다중slice
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-// action에 대한 오타를 걱정할 필요가 없어진다.
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
