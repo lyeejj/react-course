6ee970b46109a83c2a8f2eefbea83d54c7f13d17
yjlee1012 <elj308@naver.com>
2022-12-19 01:26:42 +0900

    working with multiple slices & dispatch(redux-toolkit)

--- practice Redux/src/store/index.js
+++ practice Redux/src/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -25,11 +25,30 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  // merge 다중slice
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 // action에 대한 오타를 걱정할 필요가 없어진다.
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
